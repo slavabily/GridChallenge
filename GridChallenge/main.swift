4aa295ee5044ea565fde351bdb2ffed7ed3b2197
slava bily <slavabily@me.com>
2021-11-04 20:53:08 +0200

    successful submission

--- GridChallenge/main.swift
+++ GridChallenge/main.swift
@@ -8,12 +8,26 @@
 import Foundation
 
 func gridChallenge(grid: [String]) -> String {
-    let rowSample = grid[0]
-    let rev = String(rowSample.sorted())
-    print(rev)
-
+    var rowArr = [[Character]]()
+    for i in 0..<grid.count {
+        let oneRow = Array(grid[i])
+        let rev = oneRow.sorted()
+        print(rev)
+        rowArr.append(rev)
+    }
+    print(rowArr)
+    for i in 0..<rowArr.count {
+        var oneColumn = [Character]()
+        for j in 0..<rowArr[i].count {
+            oneColumn.append(rowArr[j][i])
+        }
+        print(oneColumn)
+        if oneColumn != oneColumn.sorted() {
+            return "NO"
+        }
+    }
     return "YES"
 }
 
-print(gridChallenge(grid: ["cab","ade","efg"]))
+print(gridChallenge(grid: ["abc","ade","efg"]))
 
